7a5afb9ff1eb4429670d9186e72ee3540af9b1bf
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-14 21:32:17 +0300

    updating app file

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,12 +1,7 @@
-import React from 'react';
-
+import React from "react";
+import MyPage from "./componets/socialmedia/MyPage";
 function App() {
-  return (
-    <div className="bg-blue-600 text-white p-4 rounded-md shadow-md">
-      <h1 className="text-xl font-bold mb-2">Hello Tailwind!</h1>
-      <p className="text-sm">This is a simple component styled with Tailwind CSS.</p>
-    </div>
-  );
+  return <MyPage />;
 }
 
 export default App;
