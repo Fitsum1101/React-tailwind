f1b820db96fa5778a3bbe440a76bbc24e318c0b5
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-14 21:35:28 +0300

    updatung the finall code

--- project-1/src/componets/Blog/end.js
+++ project-1/src/componets/Blog/end.js
@@ -8,7 +8,6 @@
                 
                 <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-4">
                     <div class="flex flex-col items-center p-8 transition-colors duration-200 transform cursor-pointer group hover:bg-blue-600 rounded-xl">
-                        <img class="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="">
                         
                         <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white group-hover:text-white">arthur melo</h1>
                         
